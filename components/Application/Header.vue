71b3ace83a4c393064b89a3df739d6100aea1e5f
André Brandão <andrebf4@gmail.com>
2018-05-14 22:21:03 -0300

    clean up project and added Article Model

---
+++ components/Application/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+  <h1>André Brandão</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'component-application-header'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ models/Article.js
@@ -0,0 +1,8 @@
+export default {
+  id: 'article-01',
+  slug: 'article_title',
+  title: 'Article title',
+  thumbnail: 'https://i1.wp.com/www.techemportugues.com/wp-content/uploads/2016/12/js.png?resize=700%2C350&ssl=1',
+  previewText: 'The javascript is a programming language',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+}
