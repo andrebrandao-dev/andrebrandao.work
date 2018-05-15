a2199c910ab88413bab18d85a6c69153516171b6
André Brandão <andrebf4@gmail.com>
2018-05-14 22:27:33 -0300

    Course Model

---
+++ models/Course.js
@@ -0,0 +1,8 @@
+export default {
+  id: 'course-01',
+  slug: 'nuxt.js-vue.js-on-steroids',
+  title: 'Nuxt.js - Vue.js on Steroids',
+  institution: 'Udemy',
+  thumbnail: 'https://udemy-images.udemy.com/course/480x270/1523224_60cb.jpg',
+  certificateOfCompletion: 'https://udemy-certificate.s3.amazonaws.com/image/UC-RPO05TKJ.jpg'
+}
