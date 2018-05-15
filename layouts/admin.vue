034a90477c8771d8dc13885b28262924a327d996
André Brandão <andrebf4@gmail.com>
2018-05-14 23:06:47 -0300

    admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header />
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+  import Header from '@/components/Application/Header'
+
+  export default {
+    name: 'layout-admin',
+    components: {
+      Header
+    }
+  }
+</script>
+
+<style>
+</style>
