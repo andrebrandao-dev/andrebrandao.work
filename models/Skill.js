9952f2b4b8217c212a4c51c35631be45f00d1767
André Brandão <andrebf4@gmail.com>
2018-05-14 22:38:40 -0300

    Models: Project and Skill

---
+++ models/Skill.js
@@ -0,0 +1,9 @@
+export default {
+  id: 'skill-01',
+  slug: 'vuejs',
+  title: 'VueJs',
+  experienceTime: 1,
+  experienceType: 'ano',
+  thumbnail: 'https://i1.wp.com/www.techemportugues.com/wp-content/uploads/2016/12/js.png?resize=700%2C350&ssl=1',
+
+}
